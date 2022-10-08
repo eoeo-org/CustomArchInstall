680f51fe23dbeee1455d080b538650abd77b5b88
ced <momo9795579@gmail.com>
2022-10-08 11:30:12 +0900

    hmm

--- inside-arch-chroot.sh
+++ inside-arch-chroot.sh
@@ -20,7 +20,7 @@ locale-gen # genelate lpcale
 echo LANG=en_US.UTF-8 > /etc/locale.conf # set default language
 
 # Set Hostname
-echo "[chroot] Setting Hostname..."
+echo "[chroot] Setting Hostname($OS_HOSTNAME)..."
 echo $OS_HOSTNAME > /etc/hostname
 
 # Enable Network Manager
@@ -28,20 +28,25 @@ echo "[chroot] Enable NetworkManager..."
 systemctl enable NetworkManager
 
 # Add User
-echo "[chroot] Add User..."
+echo "[chroot] Add User($USERNAME)..."
 useradd -s /bin/bash -G wheel -m $USERNAME # add user
-(
-    echo "test"
-) | passwd $USERNAME
+echo "${USERNAME}:test" | chpasswd # change password(testing)
 
 # Install systemd-boot to boot
 echo "[chroot] Install systemd-boot..."
 bootctl --path=/boot install # install systemd-boot
+
+# Generate Boot Loader Setting
+echo "[chroot] Generating Boot Loader Setting..."
+echo "timeout 3" > /boot/loader/loader.conf # add loader setting to file
+
+# Generate Boot Entry For systemd-boot
+echo "[chroot] Generating Boot Entry..."
 echo "title Arch Linux (linux-zen)" >> /boot/loader/entries/archlinux.conf # add to entry file/1
 echo "linux /vmlinuz-linux-zen" >> /boot/loader/entries/archlinux.conf # add to entry file/2
 echo "initrd /initramfs-linux-zen.img" >> /boot/loader/entries/archlinux.conf # add to entry file/3
 echo "options root=PARTUUID=${PARTUUID} zswap.enabled=0 rootflags=subvol=@arch rw intel_pstate=no_hwp rootfstype=btrfs" >> /boot/loader/entries/archlinux.conf # add to entry file/4
-bootctl update # update systemd-boot
+bootctl update # update boot entries
 
 # End Of arch-chroot
 echo "[chroot] arch-chroot Finished"
